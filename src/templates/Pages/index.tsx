e253a4c731d8dc7474f5ac2bda44bddcecd20c36
Miguel Machado <miiguellmp@gmail.com>
2021-03-06 17:17:05 -0300

    feat(pages): criando paginas genéricas dinamicamente

--- src/templates/Pages/index.tsx
+++ src/templates/Pages/index.tsx
@@ -4,21 +4,21 @@ import LinkWrapper from 'components/LinkWrapper'
 
 import * as S from './styles'
 
-const PageTemplate = () => (
+export type PageTemplateProps = {
+  heading: string
+  body: string
+}
+
+const PageTemplate = ({ heading, body }: PageTemplateProps) => (
   <S.Content>
     <LinkWrapper href="/">
       <CloseOutline size={32} aria-label="Return to home" />
     </LinkWrapper>
 
-    <S.Heading>My Trips</S.Heading>
+    <S.Heading>{heading}</S.Heading>
 
     <S.Body>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro
-        deleniti, illum repudiandae molestiae, officiis distinctio enim
-        temporibus nam veniam quibusdam quis debitis architecto magnam a
-        perspiciatis, vel obcaecati ipsam consectetur.
-      </p>
+      <div dangerouslySetInnerHTML={{ __html: body }} />
     </S.Body>
   </S.Content>
 )
