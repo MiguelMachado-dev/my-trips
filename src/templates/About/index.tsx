89ba8e6b28e5240a0c3368a1849ad4dc30986ce3
Miguel Machado <miiguellmp@gmail.com>
2021-03-06 15:43:54 -0300

    feat(about): created about page

---
+++ src/templates/About/index.tsx
@@ -0,0 +1,26 @@
+import { CloseOutline } from '@styled-icons/evaicons-outline/CloseOutline'
+
+import LinkWrapper from 'components/LinkWrapper'
+
+import * as S from './styles'
+
+const AboutTemplate = () => (
+  <S.Content>
+    <LinkWrapper href="/">
+      <CloseOutline size={32} aria-label="Return to home" />
+    </LinkWrapper>
+
+    <S.Heading>My Trips</S.Heading>
+
+    <S.Body>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro
+        deleniti, illum repudiandae molestiae, officiis distinctio enim
+        temporibus nam veniam quibusdam quis debitis architecto magnam a
+        perspiciatis, vel obcaecati ipsam consectetur.
+      </p>
+    </S.Body>
+  </S.Content>
+)
+
+export default AboutTemplate
