06c76615b6451a3778b039007a2cb49d4d91546b
Miguel Machado <miiguellmp@gmail.com>
2021-03-07 15:12:15 -0300

    fix(link-wrapper): adicionado background em pagina de about

--- src/templates/Pages/index.tsx
+++ src/templates/Pages/index.tsx
@@ -11,7 +11,7 @@ export type PageTemplateProps = {
 
 const PageTemplate = ({ heading, body }: PageTemplateProps) => (
   <S.Content>
-    <LinkWrapper hasBackground={false} right href="/">
+    <LinkWrapper hasBackground={true} right href="/">
       <CloseOutline size={32} aria-label="Return to home" />
     </LinkWrapper>
 
